54be75e147c0b912bc63741b09ae148b492467f8
Lany Baggins <lanybaggins@gmail.com>
2025-09-14 00:22:16 -0400

    show reply content to debug

--- discord/src/utils/discord/interactionReply.js
+++ discord/src/utils/discord/interactionReply.js
@@ -1,5 +1,6 @@
 import { MessageFlags } from "discord.js"
 export default async (interaction, content, flags = undefined, options = {}) => {
+  console.log(`Replying to interaction with content: ${content}`);
   if (flags === undefined) {
     flags = MessageFlags.Ephemeral
   }
